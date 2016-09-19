cd5865b2fbb76280edf760baa7c3b99675458dc3
sophiann33 <shayet@explorance.com>
2016-09-19 13:44:01 -0400

    Add Event.Id - Switch BaseEvent Properties to Event class - delete BaseEvent

--- src/ImsGlobal.Caliper/Events/Event.cs
+++ src/ImsGlobal.Caliper/Events/Event.cs
@@ -5,22 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Newtonsoft.Json;
+using NodaTime;
 
 namespace ImsGlobal.Caliper.Events {
 	using ImsGlobal.Caliper.Entities.Agent;
 	using ImsGlobal.Caliper.Entities.Lis;
 	using ImsGlobal.Caliper.Entities.W3c;
 	using ImsGlobal.Caliper.Util;
+    using ImsGlobal.Caliper.Entities.Foaf;
 
-	/// <summary>
-	/// Default base class for Caliper events.
-	/// </summary>
-	public class Event : BaseEvent {
-        //test
-		/// <summary>
-		/// Optional - "target" - from Metric Profile
+    /// <summary>
+    /// Default base class for Caliper events.
+    /// </summary>
+    public class Event : BaseEvent {
+
+        public Event()
+        {
+            this.Context = CaliperContext.Context;
+            this.Type = EventType.Event;
+        }
+
+        /// <summary>
+		/// Required - JSON-LD context for the CaliperEvent
 		/// </summary>
-		[JsonProperty( "target", Order = 6 )]
+		[JsonProperty("@context", Order = 1)]
+        public CaliperContext Context { get; set; }
+
+        /// <summary>
+        /// Required - Type of the CaliperEvent
+        /// </summary>
+        [JsonProperty("@type", Order = 2)]
+        public EventType Type { get; set; }
+
+        /// <summary>
+        /// Required - Agent (User, System) that performed the action
+        /// </summary>
+        [JsonProperty("actor", Order = 3)]
+        public IAgent Actor { get; set; }
+
+        /// <summary>
+        /// Required - Action performed by the agent - from Metric Profile
+        /// </summary>
+        [JsonProperty("action", Order = 4)]
+        public Action Action { get; set; }
+
+        /// <summary>
+        /// Required - "Activity Context" - from Metric Profile
+        /// </summary>
+        [JsonProperty("object", Order = 5)]
+        public dynamic Object { get; set; }
+
+        /// <summary>
+        /// Optional - "target" - from Metric Profile
+        /// </summary>
+        [JsonProperty( "target", Order = 6 )]
 		public dynamic Target { get; set; }
 
 		/// <summary>
@@ -29,10 +67,22 @@ namespace ImsGlobal.Caliper.Events {
 		[JsonProperty( "generated", Order = 7 )]
 		public dynamic Generated { get; set; }
 
-		/// <summary>
-		/// EdApp context
+        /// <summary>
+		/// Required - time that the event was started at
 		/// </summary>
-		[JsonProperty( "edApp", Order = 11 )]
+		[JsonProperty("eventTime", Order = 8)]
+        public Instant? EventTime { get; set; }
+
+        /// <summary>
+        /// Optional - id of the CaliperEvent
+        /// </summary>
+        [JsonProperty("id", Order = 9)]
+        public String Id { get; set; }
+
+        /// <summary>
+        /// EdApp context
+        /// </summary>
+        [JsonProperty( "edApp", Order = 11 )]
 		public SoftwareApplication EdApp { get; set; }
 
 		/// <summary>
