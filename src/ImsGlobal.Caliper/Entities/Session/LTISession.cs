0512a851ed3a70dacf64218f2e07c1c97c2d57f4
sophiann33 <shayet@explorance.com>
2016-10-04 14:24:08 -0400

    Add LtiSession - update EntityType

---
+++ src/ImsGlobal.Caliper/Entities/Session/LTISession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using NodaTime;
+
+namespace ImsGlobal.Caliper.Entities.Session {
+	using ImsGlobal.Caliper.Entities.Foaf;
+
+	public class LtiSession : Session {
+
+		public LtiSession( string id )
+			: base( id ) {
+			this.Type = EntityType.LtiSession;
+		}
+
+		[JsonProperty("launchParameters ", Order = 31 )]
+		public Object LaunchParameters { get; set; }
+
+	}
+
+}
