649cc4ad80ab3f5b02091ca639e2aeb6a183af8f
sophiann33 <shayet@explorance.com>
2016-10-05 12:36:13 -0400

    Add Collection Entity

---
+++ src/ImsGlobal.Caliper/Entities/Collection/Collection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using NodaTime;
+
+namespace ImsGlobal.Caliper.Entities {
+
+	/// <summary>
+	/// Default base class for Caliper entities.
+	/// </summary>
+	public class Collection : Entity {
+
+		public Collection( string id )
+            : base(id)
+        {
+            this.Type = EntityType.Collection;
+        }
+
+        [JsonProperty("isPartOf", Order = 11)]
+        public Entity IsPartOf { get; set; }
+
+        [JsonProperty("items", Order = 11)]
+        public IList<Entity> Items { get; set; }
+
+    }
+
+}
