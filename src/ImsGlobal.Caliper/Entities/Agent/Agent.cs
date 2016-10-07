8cb02e9d7d7fa23e31bc4ee82bee042d0cd33216
sophiann33 <shayet@explorance.com>
2016-10-07 14:20:04 -0400

    Update Agent

--- src/ImsGlobal.Caliper/Entities/Agent/Agent.cs
+++ src/ImsGlobal.Caliper/Entities/Agent/Agent.cs
@@ -4,15 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+using NodaTime;
+
 namespace ImsGlobal.Caliper.Entities.Agent {
 	using ImsGlobal.Caliper.Entities.Foaf;
 
-	public class Agent : BaseEntity, IAgent {
+	public class Agent : IEntity, IAgent {
 
-		public Agent( string id )
-			: base( id ) {
+		public Agent( string id ){
 		}
 
-	}
+        [JsonProperty("@context", Order = 0)]
+        public string Context { get; set; }
+
+        [JsonProperty("@id", Order = 1)]
+        public string Id { get; set; }
+
+        [JsonProperty("@type", Order = 2)]
+        public IType Type { get; set; }
+
+        [JsonProperty("name", Order = 3)]
+        public string Name { get; set; }
+
+        [JsonProperty("description", Order = 4)]
+        public string Description { get; set; }
+
+        [JsonProperty("extensions", Order = 51)]
+        public Object Extensions { get; set; }
+
+        [JsonProperty("dateCreated", Order = 52)]
+        public Instant? DateCreated { get; set; }
+
+        [JsonProperty("dateModified", Order = 53)]
+        public Instant? DateModified { get; set; }
+    }
 
 }
