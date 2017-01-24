8b3a27e286430980434de098ff352a80629602f9
mgylling <markus.gylling@idpf.org>
2017-01-24 15:19:23 +0100

    Add page entity
    Add page entity, in Reading namespace (for now). Fixes #80.

---
+++ src/ImsGlobal.Caliper/Entities/Reading/Page.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImsGlobal.Caliper.Entities.Reading {
+
+	public class Page : DigitalResource {
+
+		public Page(string id) : base(id) {
+			this.Type = EntityType.Page;
+		}
+
+	}
+
+}
