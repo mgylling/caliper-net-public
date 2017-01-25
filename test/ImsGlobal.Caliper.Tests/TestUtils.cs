4e003b2b6dbd75b8581b34662dec0d2b6610540c
mgylling <markus.gylling@idpf.org>
2017-01-25 13:13:04 +0100

    Load reference files from caliper-common-fixtures
    Refactor test file load method to load files directly from the
    caliper-common-fixtures project instead of local/imported files.

--- test/ImsGlobal.Caliper.Tests/TestUtils.cs
+++ test/ImsGlobal.Caliper.Tests/TestUtils.cs
@@ -10,14 +10,38 @@ namespace ImsGlobal.Caliper.Tests {
 
 	internal static class TestUtils {
 
+		/// <summary>
+		/// Loads the reference json file from the caliper-common-fixtures project, which must be
+		/// a sibling to the caliper-net project on the filesystem.
+		/// </summary>
+		/// <returns>The reference json file.</returns>
+		/// <param name="refJsonName">Reference json name.</param>
 		public static string LoadReferenceJsonFile( string refJsonName ) {
 
-			var assembly = Assembly.GetExecutingAssembly();
-			var resourceName = "ImsGlobal.Caliper.Tests.ReferenceJson." + refJsonName + ".json";
+			//get the parent dir of the caliper-net dir
+			var startDir = new DirectoryInfo(Path.GetDirectoryName(
+				Assembly.GetExecutingAssembly().Location));
+
+			while (startDir.Parent != null) {
+				startDir = startDir.Parent;
+				if (startDir.Name.Equals("caliper-net")) {
+					startDir = startDir.Parent;
+					break;
+				}	
+			}
+
+			//find the caliper-common-fixtures sibling to caliper-net
+			var dirs = startDir.GetDirectories();
+
+			var fixturesDirInfo = Array.Find(dirs, (DirectoryInfo obj) => obj.Name.Equals("caliper-common-fixtures"));
+
+			//get and read the json file from deep inside the fixtures dir
+			var path = Path.Combine(new string[5] { fixturesDirInfo.FullName, "src", "test", "resources", "fixtures" });
+
+			var stream = new FileStream(path + Path.DirectorySeparatorChar + refJsonName + ".json", FileMode.Open); 
 
 			string content = null;
 
-			using (Stream stream = assembly.GetManifestResourceStream( resourceName ))
 			using (StreamReader reader = new StreamReader( stream )) {
 				content = reader.ReadToEnd();
 			}
