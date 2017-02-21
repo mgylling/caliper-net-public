697dc654fd9a4558ce99f581e4257c2d03435f54
mgylling <markus.gylling@idpf.org>
2017-02-21 16:19:12 +0100

    Add Assessment.items
    Add Assessment.items. Fixes #81.

--- src/ImsGlobal.Caliper/Entities/Assessment/Assessment.cs
+++ src/ImsGlobal.Caliper/Entities/Assessment/Assessment.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 namespace ImsGlobal.Caliper.Entities.Assessment {
 	using ImsGlobal.Caliper.Entities.Assignable;
 	using ImsGlobal.Caliper.Entities.Qti;
+
 
 	public class Assessment : AssignableDigitalResource, IAssignable, IAssessment {
 
 		public Assessment( string id )
 			: base( id ) {
 			this.Type = EntityType.Assessment;
+			this.Items = new List<AssessmentItem>();
 		}
 
+		[JsonProperty("items", Order = 1)]
+		public IList<AssessmentItem> Items { get; set; }
 	}
 
 }
