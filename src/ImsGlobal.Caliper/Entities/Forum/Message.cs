ee4c216b19e9ccf1b59696bdd705aaa98e9d0b84
mgylling <markus.gylling@idpf.org>
2017-04-27 16:01:03 +0200

    Fixed serialized names in Message
    Fixed serialized names in Message (remove @ char and use lowercase).

--- src/ImsGlobal.Caliper/Entities/Forum/Message.cs
+++ src/ImsGlobal.Caliper/Entities/Forum/Message.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 using Newtonsoft.Json;
-using NodaTime;
 
 namespace ImsGlobal.Caliper.Entities.Forum {
 
@@ -20,13 +15,13 @@ namespace ImsGlobal.Caliper.Entities.Forum {
             this.Type = EntityType.Message;
         }
 
-        [JsonProperty("@replyTo", Order = 20)]
+        [JsonProperty("replyTo", Order = 20)]
         public Message ReplyTo { get; set; }
 
-        [JsonProperty("@content", Order = 21)]
-        public string Content { get; set; }
+        [JsonProperty("body", Order = 21)]
+        public string Body { get; set; }
 
-        [JsonProperty("@attachments", Order = 22)]
+        [JsonProperty("attachments", Order = 22)]
         public IList<DigitalResource> Attachments { get; set; }
 
     }
