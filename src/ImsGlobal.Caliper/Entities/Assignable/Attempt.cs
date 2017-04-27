d8aceb953b7bc39f4effa1f1070f397c54f9207e
mgylling <markus.gylling@idpf.org>
2017-04-27 15:58:28 +0200

    Add Attempt.assignable and assignee.
    Add Attempt.assignable and assignee.

--- src/ImsGlobal.Caliper/Entities/Assignable/Attempt.cs
+++ src/ImsGlobal.Caliper/Entities/Assignable/Attempt.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Newtonsoft.Json;
 using NodaTime;
 
 namespace ImsGlobal.Caliper.Entities.Assignable {
-	using ImsGlobal.Caliper.Entities.Foaf;
-	using ImsGlobal.Caliper.Util;
+	using ImsGlobal.Caliper.Entities.Agent;
 
 	/// <summary>
 	/// Representation of an Attempt. Attempts are generated as part of or
@@ -22,16 +16,22 @@ namespace ImsGlobal.Caliper.Entities.Assignable {
 			this.Type = EntityType.Attempt;
 		}
 
-		[JsonProperty( "count", Order = 11 )]
+		[JsonProperty("assignable", Order = 11)]
+		public DigitalResource Assignable { get; set; }
+
+		[JsonProperty("assignee", Order = 12)]
+		public Person Assignee { get; set; }
+
+		[JsonProperty( "count", Order = 13 )]
 		public int Count { get; set; }
 
-		[JsonProperty( "startedAtTime", Order = 12 )]
+		[JsonProperty( "startedAtTime", Order = 14 )]
 		public Instant? StartedAtTime { get; set; }
 
-		[JsonProperty( "endedAtTime", Order = 13 )]
+		[JsonProperty( "endedAtTime", Order = 15 )]
 		public Instant? EndedAtTime { get; set; }
 
-		[JsonProperty( "duration", Order = 14 )]
+		[JsonProperty( "duration", Order = 16 )]
 		public Period Duration { get; set; }
 
 		[JsonProperty("isPartOf", Order = 17)]
